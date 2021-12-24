81a73492679e441e233b54ed0be1936c7cbed65e
Derek Todd <derekhtodd@yahoo.co.uk>
2021-12-24 17:43:45 +0000

    out of time - next to tidy up

--- test/test_RummySimple.py
+++ test/test_RummySimple.py
@@ -29,13 +29,11 @@ class TestRummy(unittest.TestCase):
         self.test_output.reset_list_of_test_outputs()
      
 
-    # def test_rummy(self):
-    #     self.test_input.set_list_of_test_inputs([3,"T","S"])
-    #     self.rummy.main()
 
-    # def test_valid_deal_input(self):
-    #     self.test_input.set_list_of_test_inputs(["T","S"])
-    #     self.assertEqual("T",self.rummy.valid_deal_input())
+
+
+
+
 
 
     # Test correct number of cards
@@ -61,9 +59,11 @@ class TestRummy(unittest.TestCase):
         self.assertEqual(self.deck, self.playing_card.generate_deck())
 
 
-    # def test_getNumberFromPlayer(self):
-    #     self.test_input.set_list_of_test_inputs(["S"])
-    #     self.rummy.set_game_input(self.test_input)
+
+
+
+
+
 
 
     def test_askYorN_y(self):
@@ -126,9 +126,97 @@ class TestRummy(unittest.TestCase):
 
 
 
-    def tst_
+    def test_checkForRun_NoRun(self):
+        startPosition = 1
+        playLength = 3
+        playerHand = ['C03', 'D08', 'D10', 'D11', 'H04', 'H06', 'H11', 'H12', 'S01', 'S06']
+        resultOfCheck = self.rummy.checkForRun(startPosition, playLength, playerHand)
+        self.assertEquals(resultOfCheck, [])
+
+    def test_checkForRun_YesRun_1_3(self):
+        startPosition = 1
+        playLength = 3
+        playerHand = ['C03', 'D08', 'D09', 'D10', 'D11', 'H04', 'H06', 'H11', 'H12', 'S01']
+        resultOfCheck = self.rummy.checkForRun(startPosition, playLength, playerHand)
+        self.assertEquals(resultOfCheck, ['D08', 'D09', 'D10'])
+
+    def test_checkForRun_NoRun_3_3(self):
+        startPosition = 3
+        playLength = 3
+        playerHand = ['C03', 'D08', 'D09', 'D10', 'D11', 'H04', 'H06', 'H11', 'H12', 'S01']
+        resultOfCheck = self.rummy.checkForRun(startPosition, playLength, playerHand)
+        self.assertEquals(resultOfCheck, [])
 
 
+    def test_checkForRun_YesRun_1_4(self):
+        startPosition = 1
+        playLength = 4
+        playerHand = ['C03', 'D08', 'D09', 'D10', 'D11', 'H04', 'H06', 'H11', 'H12', 'S01']
+        resultOfCheck = self.rummy.checkForRun(startPosition, playLength, playerHand)
+        self.assertEquals(resultOfCheck, ['D08', 'D09', 'D10', 'D11'])
+
+
+
+    def test_checkForSet_YesSet_3_3(self):
+        startPosition = 3
+        playLength = 3
+        playerHand = ['C03', 'C04', 'D04', 'D08', 'D11', 'H04', 'H08', 'H11', 'S04', 'S08']
+        resultOfCheck = self.rummy.checkForSet(startPosition, playLength, playerHand)
+        self.assertEquals(resultOfCheck, ['D08', 'H08', 'S08'])
+
+    def test_checkForSet_YesSet_1_3(self):
+        startPosition = 1
+        playLength = 3
+        playerHand = ['C03', 'C04', 'D04', 'D08', 'D11', 'H04', 'H08', 'H11', 'S04', 'S08']
+        resultOfCheck = self.rummy.checkForSet(startPosition, playLength, playerHand)
+        self.assertEquals(resultOfCheck, ['C04', 'D04', 'H04'])
+
+
+    def test_checkForSet_YesSet_1_4(self):
+        startPosition = 1
+        playLength = 4
+        playerHand = ['C03', 'C04', 'D04', 'D08', 'D11', 'H04', 'H08', 'H11', 'S04', 'S08']
+        resultOfCheck = self.rummy.checkForSet(startPosition, playLength, playerHand)
+        self.assertEquals(resultOfCheck, ['C04', 'D04', 'H04', 'S04'])
+
+    def test_checkForSet_NoSet_0_4(self):
+        startPosition = 0
+        playLength = 4
+        playerHand = ['C03', 'C04', 'D04', 'D08', 'D11', 'H04', 'H08', 'H11', 'S04', 'S08']
+        resultOfCheck = self.rummy.checkForSet(startPosition, playLength, playerHand)
+        self.assertEquals(resultOfCheck, [])
+
+
+
+    def test_findPlaysInHand(self):
+        listOfPlays = {'playerHand': ['C03', 'C04', 'D04', 'D08', 'D11', 'H04', 'H08', 'H11', 'S04', 'S08'], 'playsFound': [], 'newPlaysFound': [], 'playsMadeSoFar': [], 'bestScoreSoFar': 0}
+        resultOfCheck = self.rummy.findPlaysInHand(listOfPlays)
+        self.assertEquals(resultOfCheck['newPlaysFound'], [['C04', 'D04', 'H04'], ['D04', 'H04', 'S04'], ['D08', 'H08', 'S08'], ['C04', 'D04', 'H04', 'S04']])
+
+
+
+
+    def test_findPlaysInHand2(self):
+        listOfPlays = {'playerHand': ['C03', 'C04', 'C05', 'D04', 'D08', 'D11', 'H04', 'H08', 'H11', 'S04', 'S08'], 'playsFound': [], 'newPlaysFound': [], 'playsMadeSoFar': [], 'bestScoreSoFar': 0}
+        resultOfCheck = self.rummy.findPlaysInHand(listOfPlays)
+        self.assertEquals(resultOfCheck['newPlaysFound'], [['C03', 'C04', 'C05'], ['C04', 'D04', 'H04'], ['D04', 'H04', 'S04'], ['D08', 'H08', 'S08'], ['C04', 'D04', 'H04', 'S04']])
+
+
+
+    def test_findPlaysInHand_MeldSet(self):
+        listOfPlays = {'playerHand': ['C03', 'C04', 'C05', 'D04', 'D08', 'D11', 'H04', 'H08', 'H11', 'S04', 'S08'], 'playsFound': [['D05', 'H05', 'S05']], 'newPlaysFound': [], 'playsMadeSoFar': [], 'bestScoreSoFar': 0}
+        resultOfCheck = self.rummy.findPlaysInHand(listOfPlays)
+        self.assertEquals(resultOfCheck['newPlaysFound'], [['C03', 'C04', 'C05'], ['C04', 'D04', 'H04'], ['D04', 'H04', 'S04'], ['D08', 'H08', 'S08'], ['C04', 'D04', 'H04', 'S04'], [['C05'], ['D05', 'H05', 'S05', 'C05'], ['D05', 'H05', 'S05']]])
+
+    def test_findPlaysInHand_MeldSet(self):
+        listOfPlays = {'playerHand': ['C03', 'C04', 'C05', 'D04', 'D08', 'H03', 'H04', 'H08', 'H11', 'S04', 'S08'], 'playsFound': [['D09', 'D10', 'D11']], 'newPlaysFound': [], 'playsMadeSoFar': [], 'bestScoreSoFar': 0}
+        resultOfCheck = self.rummy.findPlaysInHand(listOfPlays)
+        print("Result:", resultOfCheck)
+        self.assertEquals(resultOfCheck['newPlaysFound'], [['C03', 'C04', 'C05'], ['C04', 'D04', 'H04'], ['D04', 'H04', 'S04'], ['D08', 'H08', 'S08'], ['C04', 'D04', 'H04', 'S04'], [['D08'], ['D08', 'D09', 'D10', 'D11'], ['D09', 'D10', 'D11']]])
+
+
+
+# listOfPlays = {'playerHand': ['C01', 'C03', 'C04', 'C08', 'D03', 'D09', 'D11', 'D13', 'H08', 'H12', 'H13'], 'playsFound': [], 'newPlaysFound': [], 'playsMadeSoFar': [], 'bestScoreSoFar': 0}
 
 
 
