1f33a7ec2f75cc2c4c684f7aeb04e1018fff7749
Derek Todd <derekhtodd@yahoo.co.uk>
2021-12-24 02:43:09 +0000

    add comment

--- src/ConsoleOutput.py
+++ src/ConsoleOutput.py
@@ -3,6 +3,6 @@ sys.path.append('/home/derek/Documents/UoG/GA1/FPSE/AdapterSSversion')
 from src.Output import Output
 import unittest
 class ConsoleOutput(Output):
-
+    # print() accepts a variable number of parameters
     def display(self, *message):
         return print(*message)
